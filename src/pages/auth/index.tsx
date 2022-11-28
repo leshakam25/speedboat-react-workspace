4f0ee250eb51f2e50dcfee8b5dc3fda35756bc83
leshakam25 <leshakam25@gmail.com>
2022-11-28 11:39:59 +0300

    change login page

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,44 +1,58 @@
 import * as React from "react";
-import { AuthPage as MUIAuthPage, AuthProps } from "@pankod/refine-mui";
-
+import {
+  AuthPage as MUIAuthPage,
+  AuthProps,
+  Box,
+  display,
+  Typography,
+} from "@pankod/refine-mui";
+import SailingIcon from "@mui/icons-material/Sailing";
 import { useRouterContext } from "@pankod/refine-core";
 
 const authWrapperProps = {
-    style: {
-        background:
-            "radial-gradient(50% 50% at 50% 50%,rgba(255, 255, 255, 0) 0%,rgba(0, 0, 0, 0.5) 100%),url('images/login-bg.png')",
-        backgroundSize: "cover",
-    },
+  style: {
+    background:
+      "radial-gradient(50% 50% at 50% 50%,rgba(255, 255, 255, 0) 0%,rgba(0, 0, 0, 0.5) 100%),url('images/login-bg.jpg')",
+    backgroundSize: "cover",
+  },
 };
 
 const renderAuthContent = (content: React.ReactNode) => {
-    const { Link } = useRouterContext();
+  const { Link } = useRouterContext();
 
-    return (
-        <div
-            style={{
-                margin: "auto",
-            }}
+  return (
+    <div
+      style={{
+        margin: "auto",
+      }}
+    >
+      <Link to="/">
+        <Box
+          sx={{
+            display: "flex",
+            flexdirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
         >
-            <Link to="/">
-                <img
-                    src="/images/fine-foods-login.svg"
-                    alt="fineFoods Logo"
-                    style={{ width: "100%", marginBottom: 26 }}
-                />
-            </Link>
-            {content}
-        </div>
-    );
+          <SailingIcon />{" "}
+          <Typography align="center" fontSize={38} fontWeight={600}>
+            Ladoga Rent
+          </Typography>
+        </Box>
+      </Link>
+      {content}
+    </div>
+  );
 };
 
 export const AuthPage: React.FC<AuthProps> = ({ type, formProps }) => {
-    return (
-        <MUIAuthPage
-            type={type}
-            wrapperProps={authWrapperProps}
-            renderContent={renderAuthContent}
-            formProps={formProps}
-        />
-    );
+  return (
+    <MUIAuthPage
+      type={type}
+      wrapperProps={authWrapperProps}
+      renderContent={renderAuthContent}
+      formProps={formProps}
+    />
+  );
 };
